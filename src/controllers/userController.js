d6261827ab653a0efe02562af380a32b20389b6d
jaydip-changani <pjaydip956@gmail.com>
2025-11-03 13:51:36 +0530

    get user by role

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,9 +4,14 @@ import User from "../models/userModel.js";
 // @route GET /api/users
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find().select("-password");
+    const { role } = req.query;
+    const filter = role ? { role } : {};
+
+    const users = await User.find(filter).select("-password");
+
     res.status(200).json(users);
   } catch (error) {
+    console.error("Error fetching users:", error);
     res.status(500).json({ message: error.message });
   }
 };
