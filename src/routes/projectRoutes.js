1bcc0625eb8eb20ef5bab6b5eb69e0d6c941d0a9
jaydip-changani <pjaydip956@gmail.com>
2025-11-04 14:51:46 +0530

    file upload in admin - projects

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -6,11 +6,11 @@ import {
   updateProject,
   deleteProject,
   getProjectLogs,
+  uploadProjectDocument,
 } from "../controllers/projectController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 import { upload } from "../middlewares/uploadMiddleware.js";
-import { uploadProjectDocument } from "../controllers/projectController.js";
 
 const router = express.Router();
 
@@ -19,12 +19,12 @@ router.use(protect); // all routes need authentication
 router
   .route("/")
   .get(getAllProjects)
-  .post(authorizeRoles("Admin", "ProjectManager"), createProject);
+  .post(authorizeRoles("Admin", "ProjectManager"),upload.array("files", 10), createProject);
 
 router
   .route("/:id")
   .get(getProjectById)
-  .put(authorizeRoles("Admin", "ProjectManager"), updateProject)
+  .put(authorizeRoles("Admin", "ProjectManager"),upload.array("files", 10), updateProject)
   .delete(authorizeRoles("Admin", "ProjectManager"), deleteProject);
 
 router.get("/:id/logs", getProjectLogs);
@@ -32,7 +32,7 @@ router.get("/:id/logs", getProjectLogs);
 router.post(
   "/:id/upload",
   protect,
-  upload.single("document"),
+  upload.array("files", 10),
   uploadProjectDocument
 );
 
